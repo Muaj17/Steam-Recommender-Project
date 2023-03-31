61cc87c05d159190b2912de35acf7c493fcb5f8d
Muaj17 <67396297+Muaj17@users.noreply.github.com>
2023-03-31 06:54:05 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import game_graph_csv
 
 class TestReadCsv:
     def test_read_data_csv(self):
-        games = game_graph_csv.read_data_csv('datasets/game_simple.csv')
+        games = game_graph_csv.read_data_csv('datasets/games.csv')
         print(games)
         for game in games:
             print(games[game].game_id, games[game].name, games[game].genres, games[game].operating_systems,
                   games[game].price, games[game].date_release, games[game].rating)
 
     def test_read_metadata_csv(self):
-        result = game_graph_csv.read_metadata_json('datasets/game_metadata_simple.json')
+        result = game_graph_csv.read_metadata_json('datasets/games_metadata.json')
         print(result)
         assert type(result[0][0]) == int
 
